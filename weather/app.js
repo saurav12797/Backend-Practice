9d19dc5bf8b8871d6a4fc5d82cbe255d2ceca6ca
saurav <saurav12797@gmail.com>
2021-05-14 17:17:24 +0530

    GUI Added

--- weather/app.js
+++ weather/app.js
@@ -5,10 +5,15 @@ const https=require("https");
 const app = express();
 
 app.get("/",function(req,res)
-{
-    const url="https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=a254af5db767af45f14e8a85fa53c6e6&units=metric";
 
-    https.get(url,function(response)   //https.get(url,call backfunction) CALL BACK FUNCTION GIVES THE RESPONSE
+{
+
+  const query="Delhi";
+  const unit="metric";
+    const apiKey="a254af5db767af45f14e8a85fa53c6e6";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+
+    https.get(url,function(response)   //https.get(url,call backfunction) CALL BACK FUNCTION G IVES THE RESPONSE
     {
          
       console.log(response.statusCode) ;//status code gives error to identify like 200- ok  404 - sever not found 
@@ -24,44 +29,14 @@ app.get("/",function(req,res)
           const humidity=WeatherData.main.humidity;
           const  speed=WeatherData.wind.speed;
           const  name=WeatherData.name;
-
-         /*  const icon=WeatherData.weather[0].icon;
-
-          const imageURL="http://openweathermap.org/img/wn/"+ icon +"@2x.png";
-
-          res.write("<img src="+ imageURL+">"); */
+          
 
            res.write(`<h1>Temperature of ${name} is ${temp} degree<h1>`);
            res.write(`<h2>The wind is blowing at speed of ${speed} and humidity is ${humidity} <h2>`);
            res.send();
           
 
-          
-
-           
-
-          //or
-
-        // res.write("humidity is "+humidity)
-        //res.write("speed is"+ speed)
-        
-
-
-
-         //console.log(WeatherData);
-
-         //OR using OBJECTS
-/* 
-            const object={
-              name:"saurav", 
-              fav_food:"pizza"
-
-            }
-            console.log(JSON.stringify(object)); //converting into single string JSON FILE */
-
-
-
-
+      
        })
     })
 })
