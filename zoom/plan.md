e79c79d37f926d9c9dd6c0ee250553dedcbc7cd4
saurav <saurav12797@gmail.com>
2021-05-18 19:11:19 +0530

    starterfile

---
+++ zoom/plan.md
@@ -0,0 +1,12 @@
+#plan of action
+
+-initialize not eproject
+-initialize our first view
+- create room id
+-add ability to owm video
+
+-add ability to streaam their video
+- Add styling
+- ability to create message
+-add mute  button
+-add stop button
---
+++ zoom/server.js
@@ -0,0 +1,33 @@
+//ejs helps to get variable from frontend to backend
+
+
+const express= require('express');
+const app=express(); //allows to build application
+
+const server= require('http').Server(app);
+app.set('view engine','ejs'); //set ejs as defult
+ 
+app.get('/',(req,res)=>{
+
+
+res.render('room'); //just render the room file
+
+
+
+
+   
+
+//res.status(200).send("hello world"); //This function accepts single parameter code that holds the HTTP status code. and 200 stands for everything os ok
+
+
+})
+
+
+
+
+server.listen(3000,function(){
+    console.log("Server started");
+});
+
+
+
