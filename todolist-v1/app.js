ed63236267676ef32417515af77041f7b039f9ea
saurav <saurav12797@gmail.com>
2021-05-20 09:01:33 +0530

    added switch case

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,40 +1,49 @@
-const express= require("express");
-const bodyParser= require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app= express();
+const app = express();
 
-app.set('view engine', 'ejs'); // always below express // using of ejs with express module
+app.set("view engine", "ejs"); // always below express // using of ejs with express module
 
 //our view engine by default will look into view folder
 
-app.get("/",function(req,res)
-{
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
- var today=new Date();
- var currentDay= today.getDay();
- var day="";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thusday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+      case 6:
+        day = "Saturday";
+        break;
 
-  if(currentDay === 6 || currentDay === 0)
-  {
-      day="Weekend";
-
-   // here we are rending the file list and passing to kindOfDay which is present in list.ejs as day variable
+    default:
+    console.log("error");
   }
-  else
-  {
-    day="Weekday";
-    
-  }
-  
-  res.render("list",{kindOfDay:day}) ;// earlier -res.sendFile(__dirname,"/weekend.html");
 
+ 
 
-
-
+  res.render("list", { kindOfDay: day }); // earlier -res.sendFile(__dirname,"/weekend.html");
 });
 
-
-app.listen(3000,function(){
-
-    console.log("server started");
-})
+app.listen(3000, function () {
+  console.log("server started");
+});
